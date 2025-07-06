d65273898f18ea628e6a8fc6516203c631818216
Cosmos20016 <keviinborja8@gmail.com>
2025-07-06 14:23:50 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,6 +22,7 @@ import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.
 import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
+import { pluginLanguageBadge } from "./src/plugins/expressive-code/language-badge";
 
 // https://astro.build/config
 export default defineConfig({
