fe734a6d8d03d04030fa60bcf4ac4b9e2a995f9c
Cosmos20016 <keviinborja8@gmail.com>
2025-07-06 16:24:52 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,5 +3,3 @@ import { vitePreprocess } from "@astrojs/svelte";
 export default {
   preprocess: [vitePreprocess({ script: true })],
 };
-import demoBanner from '../assets/images/demo-banner.png';
-<img src={demoBanner} alt="demo banner" />
