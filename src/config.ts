d433df56f8f3f2a14d43c684f5c245880fa29a34
Cosmos20016 <keviinborja8@gmail.com>
2025-07-08 01:11:02 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -15,8 +15,6 @@ export const siteConfig: SiteConfig = {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
-  // ... (otras propiedades si las tienes)
-}
 	banner: {
 		enable: true,
 		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
