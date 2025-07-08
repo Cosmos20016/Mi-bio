9d0eb7a7f79509a23bb8d61180c53deb45d568a6
Cosmos20016 <keviinborja8@gmail.com>
2025-07-08 01:09:39 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Kevin Borja", // para pone subtitulo poner debajo: subtitle: "🐢",
+	title: "Kevin Borja",
+	subtitle: "🐢",
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
