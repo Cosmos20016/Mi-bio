5d5abcfa3cb9118cc41fc0e901db453987aa87ab
Cosmos20016 <keviinborja8@gmail.com>
2025-07-08 01:07:21 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,14 +8,12 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: "Kevin Borja",
-  subtitle: "🐢",
-  showSubtitle: false, // <--- Nuevo campo
-  lang: "es", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
-  themeColor: {
-    hue: 250, // Default hue for the theme color, from 0 to 360.
-    fixed: false, // Hide the theme color picker for visitors
-  },
+	title: "Kevin Borja",
+	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+	themeColor: {
+		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		fixed: false, // Hide the theme color picker for visitors
+	},
   // ... (otras propiedades si las tienes)
 }
 	banner: {
