d0e7f4a64ebb85e796f1cb9f8fa03c3b2cce4139
Kevin Borja <keviinborja8@gmail.com>
2025-12-06 00:49:26 -0500

    Update avatar path to be absolute

--- src/config.ts
+++ src/config.ts
@@ -58,7 +58,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.mp4", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "/assets/images/demo-avatar.mp4", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Kevin Borja",
 	bio: "Sencillo, creativo, yo",
 	links: [
